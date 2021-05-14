eb741c58f861b495c9949bf3d447e1e8104437a2
Alex Knopach <alexanderknopach@gmail.com>
2021-05-14 22:31:24 +0300

    feat: implement countPairs:number: method

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger counter = 0;
+    for (NSInteger i = array.count - 1; i > 0; i--) {
+        for (NSInteger j = 0; j < array.count - 1; j++) {
+            if (array[i].integerValue - array[j].integerValue == number.integerValue) {
+                counter++;
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end

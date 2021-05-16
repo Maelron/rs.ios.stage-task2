e893d35599a740841e8edf14b9b1dbec8f7c1fc8
Alex Knopach <alexanderknopach@gmail.com>
2021-05-16 04:38:55 +0300

    fix: implement correct countPairs:number: method

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -5,7 +5,7 @@
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     NSInteger counter = 0;
     for (NSInteger i = array.count - 1; i > 0; i--) {
-        for (NSInteger j = 0; j < array.count - 1; j++) {
+        for (NSInteger j = i-1; j >= 0; j--) {
             if (array[i].integerValue - array[j].integerValue == number.integerValue) {
                 counter++;
             }

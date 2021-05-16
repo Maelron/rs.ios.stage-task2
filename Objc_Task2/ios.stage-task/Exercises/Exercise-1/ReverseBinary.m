0aacc9d62ed48069de2d1e4b4317760dbd1d2e21
Alex Knopach <alexanderknopach@gmail.com>
2021-05-16 19:30:38 +0300

    feat: implement ReverseInteger task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    UInt8 reversedRes = 0;
+
+    for (int i = 0; i < 8; i++) {
+        reversedRes = reversedRes | (n&1) << (7 - i);
+        n = n >> 1;
+    }
+    
+    return reversedRes;
 }

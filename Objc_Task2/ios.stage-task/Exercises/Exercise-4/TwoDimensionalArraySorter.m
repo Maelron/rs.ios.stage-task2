01b1e4b7a4eb4b7d0c5d2b2dc557852128055b4e
Alex Knopach <alexanderknopach@gmail.com>
2021-05-16 04:39:39 +0300

    feat: implement twoDimensionalSort: method

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,41 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (![array.firstObject isKindOfClass:[NSArray class]]) {
+        NSLog(@"first element is not an array");
+        return @[];
+    }
+    
+    NSMutableArray *arrOfNums = [NSMutableArray new];
+    NSMutableArray *arrOfStrs = [NSMutableArray new];
+    
+    for (NSArray *subArray in array) {
+        for (id item in subArray) {
+            if ([subArray.firstObject isKindOfClass:[NSNumber class]]) {
+                [arrOfNums addObject:item];
+            } else {
+                [arrOfStrs addObject:item];
+            }
+        }
+    }
+    
+    if ([arrOfStrs isEqualToArray:@[]]) {
+        return [[NSArray arrayWithArray:arrOfNums] sortedArrayUsingSelector:@selector(compare:)];
+    }
+    
+    if ([arrOfNums isEqualToArray:@[]]) {
+        return [[NSArray arrayWithArray:arrOfStrs] sortedArrayUsingSelector:@selector(compare:)];
+    }
+    
+    NSArray *sortedNumArr = [arrOfNums sortedArrayUsingSelector:@selector(compare:)];
+    NSEnumerator *enumer = [[[NSArray arrayWithArray:arrOfStrs] sortedArrayUsingSelector:@selector(compare:)] reverseObjectEnumerator];
+    
+    NSMutableArray *combiningArr = [NSMutableArray new];
+    [combiningArr addObject:sortedNumArr];
+    [combiningArr addObject:[enumer allObjects]];
+    
+    return combiningArr;
+//    return [NSArray arrayWithArray:combiningArr];
 }
 
 @end
